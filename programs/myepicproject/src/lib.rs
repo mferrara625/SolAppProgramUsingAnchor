6893a27d3b9b74b31e05c69203f58d58baad56ae
mferrara625 <mferrara401@yahoo.com>
2023-01-11 19:38:47 -0500

    deployed to devnet

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -1,7 +1,6 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("FHbBQDoHrMos9tKTMt9C5N2hqgM7Xhm9wRtkNLfFV1iB");
 
 #[program]
 pub mod myepicproject {
